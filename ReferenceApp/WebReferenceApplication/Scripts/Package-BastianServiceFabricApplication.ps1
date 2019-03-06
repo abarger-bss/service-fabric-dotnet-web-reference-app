5559f3e0fac5ea15b296f71c4756e5e50c2b5cc4
Andrew Barger <abarger@bastiansolutions.com>
2019-03-06 11:47:52 -0500

    Add additional script utilities

---
+++ ReferenceApp/WebReferenceApplication/Scripts/Package-BastianServiceFabricApplication.ps1
@@ -0,0 +1,43 @@
+﻿#
+# Package_BastianServiceFabricApplication.ps1
+#
+
+[cmdletbinding()]
+param(
+	$ApplicationRootPath = "..\..\"
+)
+
+$ResolvedPath = Resolve-Path $ApplicationRootPath
+
+Write-Verbose "Application path is [$ResolvedPath]"
+
+$toolsPath = Join-Path $ResolvedPath "tools"
+$nugetPath = Join-Path $toolsPath "nuget.exe"
+$sln = Join-Path $ResolvedPath "WebReferenceApp.sln"
+
+Write-Verbose "Cleaning solution $sln"
+
+msbuild $sln /target:clean /toolsversion:15.0 /p:Platform=x64 /p:Configuration=Debug
+
+if(-not (test-path $toolsPath))
+{
+	New-Item -ItemType Directory $toolsPath
+}
+
+if(-not (test-path $nugetPath))
+{
+	Write-Verbose "Downloading nuget to [$nugetPath]"
+	$url = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"	
+	$wc = [System.Net.WebClient]::new()
+	$wc.DownloadFile($url, $nugetPath)
+}
+
+Write-Verbose "Restoring nuget packages"
+
+$nugetParams = 'restore', "$ApplicationRootPath\WebReferenceApplication\packages.config", '-PackagesDirectory', "$ApplicationRootPath\packages\"
+
+& $nugetPath $nugetParams
+
+Write-Verbose "Building solution $sln and generating service fabric Application package"
+
+msbuild $sln /target:build /toolsversion:15.0 /p:Platform=x64 /p:Configuration=Debug /p:ForcePackageTarget=true
---
+++ ReferenceApp/WebReferenceApplication/Scripts/Remove-BastianServiceFabricApplication.ps1
@@ -0,0 +1,63 @@
+﻿#
+# Remove_BastianServiceFabricApplication.ps1
+#
+[CmdletBinding()]
+param(
+	[string]$AppName,
+	[string]$AppTypeName,
+	[string]$AppTypeVersion,
+	[string]$ClusterAddress = "127.0.0.1:19000",
+	[switch]$UnregisterApplicationType,
+	[switch]$ForceRemove,
+	[int]$TimeoutSec = 30
+)
+
+Write-Verbose "Connecting to Service Fabric cluster at [$ClusterAddress]"
+
+#Create a connection to the cluster. Can't perform any admin operations without this
+Connect-ServiceFabricCluster -ConnectionEndpoint $ClusterAddress
+
+Write-Verbose "Removing Service Fabric application [$AppName]"
+
+$RemoveServiceFabricApplicationArgs = @{
+	ApplicationName = $AppName
+	TimeoutSec = $TimeoutSec
+	ForceRemove = $ForceRemove
+	Force = $true
+}
+
+Remove-ServiceFabricApplication @RemoveServiceFabricApplicationArgs
+
+
+
+if($UnregisterApplicationType)
+{
+	Write-Verbose "Unregistering application type [$AppTypeName] with version [$AppTypeVersion]"
+
+	$UnregisterServiceFabricApplicationTypeArgs = @{
+		ApplicationTypeName = $AppTypeName
+		ApplicationTypeVersion = $AppTypeVersion
+		Force = $true
+		Async = $true		
+	}
+
+	Unregister-ServiceFabricApplicationType @UnregisterServiceFabricApplicationTypeArgs
+
+	while($true)
+	{
+		$appType = Get-ServiceFabricApplicationType -ApplicationTypeName $AppTypeName -ApplicationTypeVersion $AppTypeVersion
+		$status = $app.Status
+
+		Write-Verbose "Status of application type [$AppTypeName] with version [$AppTypeVersion] is [$Status]"
+
+		if($status -eq $null)
+		{
+			break;
+		}
+		else
+		{
+			Start-Sleep -Seconds 1
+		}
+
+	}
+}
